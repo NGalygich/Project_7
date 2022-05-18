3fcadbb65b0e51fa8380f7e29fa36065af6998f0
NGalygich <NGalygich@yandex.ru>
2022-05-19 01:08:36 +0300

    Add array oject ctruct

--- Program.cs
+++ Program.cs
@@ -2,6 +2,37 @@
 
 class Program{
     static public void Main(){
-       
+        string path = @"Information_Employees.txt";
+        List<Worker> workers = new List<Worker>();
+        using (StreamReader readFile = new StreamReader(path))
+        {
+            string? stringFromFile;
+            while ((stringFromFile = readFile.ReadLine()) != null){
+                string[] line  = stringFromFile.Split('#');
+                Worker worker = new Worker();
+                worker.Id = Convert.ToInt32(line[0]);
+                worker.Date = Convert.ToDateTime(line[1]);
+                worker.Fullname = line[2];
+                worker.Age = Convert.ToByte(line[3]);
+                worker.Height = Convert.ToByte(line[4]);
+                worker.Birthday = Convert.ToDateTime(line[5]);
+                worker.Birthplace = line[6];
+                workers.Add(worker);
+            }
+        }
+        //Menu();
+        /*
+        Worker[] w = new Worker[4];
+        for(int i=0;i<4;i++){
+            w[i] = new Worker();
+        }
+        w[0].age = 22;
+        */
+       //Worker w = new Worker();
+       //w.Print()
     }
+    static public void Menu(){
+
+    }
+
 }
---
+++ Worker.cs
@@ -0,0 +1,30 @@
+namespace Project_7;
+
+struct Worker{
+    private int id;
+    private DateTime date, birthDay;
+    private string fullName;
+    private byte age;
+    private byte height;
+    private string birthPlace;
+    public int Id{ get{return id;} set{id = value;}}
+    public DateTime Date{ get{return date;} set{date = value;}}
+    public DateTime Birthday{ get{return birthDay;} set{birthDay = value;}}
+    public string Fullname{ get{return fullName;} set{fullName = value;}}
+    public byte Age{ get{return age;} set{age = value;}}
+    public byte Height{ get{return height;} set{height = value;}}
+    public string Birthplace{ get{return birthPlace;} set{birthPlace = value;}}
+
+    public Worker (int id, string fullName, byte age, byte height, DateTime birhtDay, DateTime date, string birthPlace){
+        this.id = id;
+        this.fullName = fullName;
+        this.height = height;
+        this.birthDay = birhtDay;
+        this.birthPlace = birthPlace;
+        this.age = age;
+        this.date = date;
+    }
+
+    
+   
+}
